3d28d78ba5a9d57466c2b78e18e4ce6695bcfe38
Martin Acosta <tinchoz49@gmail.com>
2020-06-04 11:49:14 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# level-bench
+
+## Usage
+
+```
+$ git clone https://github.com/tinchoz49/level-bench
+$ cd level-bench
+$ npm install
+$ npm run benchmark:node
+$ npm run benchmark:browser
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+const { Suite } = require('@dxos/benchmark-suite')
+const level = require('level-mem')
+
+;(async () => {
+  const suite = new Suite()
+  const db = level()
+  const n = 1000
+
+  suite.beforeAll(async () => {
+    const op = [...Array(n).keys()].map(i => ({
+      type: 'put',
+      key: `key/${i}`,
+      value: `value/${i}`
+    }))
+
+    await db.batch(op)
+  })
+
+  suite.test('createReadStream', async () => {
+    let i = 0
+    const stream = db.createReadStream()
+    stream.on('data', () => i++)
+    await new Promise(resolve => stream.on('end', resolve))
+    if (i !== n) throw new Error('missing messages')
+  })
+
+  const result = await suite.run()
+
+  suite.print(result)
+
+  process.exit(0)
+})()
