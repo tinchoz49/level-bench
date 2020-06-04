adcbfa5b8ee703047240f4709823a7fbc99522d9
Martin Acosta <tinchoz49@gmail.com>
2020-06-04 12:00:55 -0300

    Added level

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { Suite } = require('@dxos/benchmark-suite')
-const level = require('level-mem')
+const level = require('level')
+const levelmem = require('level-mem')
 
 ;(async () => {
   const suite = new Suite()
-  const db = level()
+  const db = level('.bench')
+  const dbmem = levelmem()
   const n = 1000
 
   suite.beforeAll(async () => {
@@ -13,18 +15,35 @@ const level = require('level-mem')
       value: `value/${i}`
     }))
 
-    await db.batch(op)
+    await Promise.all([
+      db.batch(op),
+      dbmem.batch(op)
+    ])
   })
 
-  suite.test('read by get', async () => {
+  suite.test('level: read by get', async () => {
     for (let i = 0; i < n; i++) {
       await db.get(`key/${i}`)
     }
   })
 
-  suite.test('createReadStream', async () => {
+  suite.test('level: createReadStream', async () => {
     let i = 0
     const stream = db.createReadStream()
+    stream.on('data', () => i++)
+    await new Promise(resolve => stream.on('end', resolve))
+    if (i !== n) throw new Error('missing messages')
+  })
+
+  suite.test('level-mem: read by get', async () => {
+    for (let i = 0; i < n; i++) {
+      await dbmem.get(`key/${i}`)
+    }
+  })
+
+  suite.test('level-mem: createReadStream', async () => {
+    let i = 0
+    const stream = dbmem.createReadStream()
     stream.on('data', () => i++)
     await new Promise(resolve => stream.on('end', resolve))
     if (i !== n) throw new Error('missing messages')
